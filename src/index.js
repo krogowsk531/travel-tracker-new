b6726ccd132b9179e7da74f23deb6186cb44b7ca
Katy St Sauveur <katyrogowski@yahoo.com>
2020-09-19 15:25:35 -0700

    Fetch for trips working again

--- src/index.js
+++ src/index.js
@@ -7,29 +7,77 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-import ApiFetch from './ApiFetch';
-import domUpdates from './domUpdates';
+// import ApiFetch from './ApiFetch';
+// import domUpdates from './domUpdates';
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
-function getData() {
-  const api = new ApiFetch();
-  const travelerData = api.getTravelersData()
-  const tripData = api.getTripsData()
-  const destinationData = api.getDestinationsData();
+// function getData() {
+//   const api = new ApiFetch();
+//   const travelerData = api.getTravelersData()
+//   console.log("TD", travelerData)
+//   const tripData = api.getTripsData()
+//   const destinationData = api.getDestinationsData();
+//
+//   Promise.all([travelerData, tripData, destinationData])
+//     .then(data => data = {
+//       travelerData: data[0].travelers,
+//       tripData: data[0].trips,
+//       destinationData: data[0].destinations
+//     })
+//     .then(getUser)
+//     .then(getDate)
+//     // .then(displayTrips)
+//     .then(dataSets => console.log("ENDRESULT", dataSets))
+//
+//     // .catch(err => console.error(err))
+// }
 
-  Promise.all([travelerData, tripData, destinationData])
-    .then(data => data = {
-      travelerData: data[0].travelers,
-      tripData: data[0].trips,
-      destinationData: data[0].destinations
-    })
+function getData() {
+  Promise.all([
+    // fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers'),
+    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips'),
+
+    // fetch('https://fe-apps.herokuapp.com/api/v1/fitlit/1908/activity/activityData'),
+    // fetch('https://fe-apps.herokuapp.com/api/v1/fitlit/1908/hydration/hydrationData')
+  ])
+    .then(responses => Promise.all(responses.map(response => response.json())))
     .then(getUser)
     .then(getDate)
     // .then(displayTrips)
-    .then(dataSets => console.log("ENDRESULT", dataSets))
+    .then(data => console.log("ENDRESULT", data))
 
     // .catch(err => console.error(err))
 }
 
 getData()
+
+function getUser(userData) {
+  // console.log("DIE", userData)
+  // console.log("HELLO", userData[0].trips)
+  //match the userID to the loginID
+  const loggedInUser = 50;
+  const matchUser = userData[0].trips.filter(user => {
+    // console.log(user)
+    return user.userID === loggedInUser
+  })
+  // console.log("MATCH", matchUser)
+  return matchUser
+}
+
+const currentDay = Date.now()
+
+function getDate(userData) {
+  // console.log("Obj", userData)
+
+  const day = userData.reduce((acc, trip) => {
+    const pastDay = Date.parse(trip.date)
+    if (currentDay < pastDay) {
+      acc.upcoming.push(trip)
+    } else {
+      acc.past.push(trip)
+    }
+    return acc
+  }, {'past': [], 'upcoming': []})
+  return day;
+}
