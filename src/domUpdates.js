5bef557f9e2cec5db36d88a66cc8e3e4bed8c4b9
Katy St Sauveur <katyrogowski@yahoo.com>
2020-09-19 10:56:37 -0700

    Add folder to handle api fetching

---
+++ src/domUpdates.js
@@ -0,0 +1,27 @@
+const domUpdates = {
+
+  logIn() {
+    let traveler = [
+      {
+        username: 'traveler50',//id is the users id //string split and pull out 50 when you need the id
+        password: 'travel2020'
+      }
+    ]
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value
+    for (i = 0; i < traveler.length; i++) {
+      if (username == traveler[i].username && password == traveler[i].password) {
+        console.log(username + ' is logged in!!!')
+        // document.location = "/dashboard.html" make something hide instead
+        return
+      }
+    }
+    console.log('incorrect username or password')
+    alert("WRONG PASSWORD");
+    // getInfoTraveler();
+    // getInfoAgency();
+    // document.location = "/agency-dashboard"
+  }
+}
+
+export default domUpdates;
--- src/index.js
+++ src/index.js
@@ -7,4 +7,9 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+import ApiFetch from './ApiFetch';
+import domUpdates from './domUpdates';
+
 console.log('This is the JavaScript entry file - your code begins here.');
+
+domUpdates.logIn()
