a42aa004fd3c4477dad16990359ece9f90bd4628
Katy St Sauveur <katyrogowski@yahoo.com>
2020-09-20 07:59:30 -0700

    Api repo officially working in connection with fetch

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,27 +1,34 @@
 const domUpdates = {
 
-  logIn() {
-    let traveler = [
-      {
-        username: 'traveler50',//id is the users id //string split and pull out 50 when you need the id
-        password: 'travel2020'
-      }
-    ]
-    let username = document.getElementById('username').value
-    let password = document.getElementById('password').value
-    for (i = 0; i < traveler.length; i++) {
-      if (username == traveler[i].username && password == traveler[i].password) {
-        console.log(username + ' is logged in!!!')
-        // document.location = "/dashboard.html" make something hide instead
-        return
-      }
-    }
-    console.log('incorrect username or password')
-    alert("WRONG PASSWORD");
-    // getInfoTraveler();
-    // getInfoAgency();
-    // document.location = "/agency-dashboard"
-  }
+  displayWelcome(traveler) {
+    console.log("T", traveler)
+     const welcome = document.querySelector('.welcome');
+     return welcome.innerHTML += `Welcome ${traveler.user.name.split(" ")[0]}!`;
+   },
 }
+
+//   logIn() {
+//     let traveler = [
+//       {
+//         username: 'traveler50',//id is the users id //string split and pull out 50 when you need the id
+//         password: 'travel2020'
+//       }
+//     ]
+//     let username = document.getElementById('username').value
+//     let password = document.getElementById('password').value
+//     for (i = 0; i < traveler.length; i++) {
+//       if (username == traveler[i].username && password == traveler[i].password) {
+//         console.log(username + ' is logged in!!!')
+//         // document.location = "/dashboard.html" make something hide instead
+//         return
+//       }
+//     }
+//     console.log('incorrect username or password')
+//     alert("WRONG PASSWORD");
+//     // getInfoTraveler();
+//     // getInfoAgency();
+//     // document.location = "/agency-dashboard"
+//   }
+// }
 
 export default domUpdates;
--- src/index.js
+++ src/index.js
@@ -7,59 +7,58 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-// import ApiFetch from './ApiFetch';
-// import domUpdates from './domUpdates';
+import ApiFetch from './ApiFetch';
+import domUpdates from './domUpdates';
 import traveler from './traveler';
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
-// function getData() {
-//   const api = new ApiFetch();
-//   const travelerData = api.getTravelersData()
-//   console.log("TD", travelerData)
-//   const tripData = api.getTripsData()
-//   const destinationData = api.getDestinationsData();
-//
-//   Promise.all([travelerData, tripData, destinationData])
-//     .then(data => data = {
-//       travelerData: data[0].travelers,
-//       tripData: data[0].trips,
-//       destinationData: data[0].destinations
-//     })
-//     .then(getUser)
-//     .then(getDate)
-//     // .then(displayTrips)
-//     .then(dataSets => console.log("ENDRESULT", dataSets))
-//
-//     // .catch(err => console.error(err))
-// }
-
 function getData() {
-  Promise.all([
-    // fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers'),
-    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips'),
-    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
+  const api = new ApiFetch();
+  const tripData = api.getTripsData()
+  const travelerData = api.getTravelersData()
+  console.log("TD", travelerData)
+  const destinationData = api.getDestinationsData();
 
-    // fetch('https://fe-apps.herokuapp.com/api/v1/fitlit/1908/activity/activityData'),
-    // fetch('https://fe-apps.herokuapp.com/api/v1/fitlit/1908/hydration/hydrationData')
-  ])
-    .then(responses => Promise.all(responses.map(response => response.json())))
+  Promise.all([travelerData, tripData, destinationData])
+    .then(data => data = {
+      tripData: data[1].trips,
+      travelerData: data[0].travelers,
+      destinationData: data[2].destinations
+    })
     .then(getUser)
     .then(getDate)
-    .then(displayPastTrips)
+    // .then(displayTrips)
     .then(data => console.log("ENDRESULT", data))
 
     // .catch(err => console.error(err))
 }
 
+// function getData() {
+//   Promise.all([
+//     fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips'),
+//     fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers'),
+//     fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
+//   ])
+//     .then(responses => Promise.all(responses.map(response => response.json())))
+//     .then(getUser)
+//     .then(getDate)
+//     // .then(display)
+//     // .then(displayPastTrips)
+//     .then(data => console.log("ENDRESULT", data))
+//
+//     // .catch(err => console.error(err))
+// }
+
 getData()
 
-function getUser(userData) {
-  console.log("DIE", userData)
-  // console.log("HELLO", userData[0].trips)
+function getUser(data) {
+  console.log("DIE", data.tripData)
+  // console.log("HELLO", tripData[1].trips)
+  // console.log("HELLO2", userData[0].destinations)
   //match the userID to the loginID
   const loggedInUser = 50;
-  const matchUser = userData[0].trips.filter(user => {
+  const matchUser = data.tripData.filter(user => {
     // console.log(user)
     return user.userID === loggedInUser
   })
@@ -67,12 +66,12 @@ function getUser(userData) {
   return matchUser
 }
 
-const currentDay = Date.now()
 
-function getDate(userData) {
-  // console.log("Obj", userData)
+function getDate(data) {
+  console.log("Obj", data)
 
-  const day = userData.reduce((acc, trip) => {
+  const currentDay = Date.now()
+  const day = data.reduce((acc, trip) => {
     const pastDay = Date.parse(trip.date)
     if (currentDay < pastDay) {
       acc.upcoming.push(trip)
@@ -84,13 +83,26 @@ function getDate(userData) {
   return day;
 }
 
+// function getId() {
+//   const username = document.getElementById('username').value;
+//   if (username.includes('traveler')) {
+//     const travelerID = Number(username.split('traveler')[1]);
+//     return Number(travelerID);
+//   }
+// }
+
+// function display(traveler) {
+//   domUpdates.displayWelcome(traveler)
+// }
+
 function displayPastTrips(userData) {
+  // console.log("TIRED")
   console.log("BEtTer", userData)
   let displayPastTrips = document.querySelector('.past-trips-card')
   displayPastTrips.innerHTML +=
-  ` <p>Madrid</p>
+  ` <p>${userData[2].destinations}</p>
     <img>IMG</img>
-    <p>${userData.past.date}</p>
+    <p>${userData[0].past.date}</p>
     <p>Duration</p>`
     return displayPastTrips
 }
